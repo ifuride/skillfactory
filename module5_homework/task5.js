14900f3c7166d514865514f1043db9e757e7140d
«ifUride» <ifuride@gmail.com>
2021-11-11 21:48:45 +0300

    Stop outputting index to console (task 5).

--- module5_homework/task5.js
+++ module5_homework/task5.js
@@ -2,8 +2,6 @@
 //Необходимо вывести количество элементов массива, затем перебрать его и вывести в консоль каждый элемент массива.
 let arr=[1,5,10];
 console.log(arr.length);
-console.log(arr);
-//or ?
 arr.forEach(function(item, index, array) {
-    console.log(item, index);
+    console.log(item);
 });
